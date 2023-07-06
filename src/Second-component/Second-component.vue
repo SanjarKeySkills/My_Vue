287e32740c1d51821e61781cc783acfcc3962489
SanjarKeySkills <forest2004@mail.ru>
2023-07-06 11:11:34 +0600

    add new second component

---
+++ src/Second-component/Second-component.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="(todo, idx) in todos" :key="idx">{{ todo.todo }}</li>
+    </ul>
+</template>
+
+<script>
+export default (await import('vue')).defineComponent({
+    name: 'TodoList',
+    props: {
+        props: Array
+    },
+    mounted() {
+        console.log(this.todos)
+    }
+})
+</script>
