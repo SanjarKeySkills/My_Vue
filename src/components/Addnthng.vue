52a71e9f5a911c73e6f06568cb18bd30c12dbe70
SanjarKeySkills <forest2004@mail.ru>
2023-07-26 15:41:45 +0600

    add new component

---
+++ src/components/Addnthng.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input v-model="todoValue1" type="text">
+        <button>App</button>
+    </div>
+</template>
+
+<script>
+
+export default (await import('vue')).defineComponent({
+    name: 'AddTodo',
+    data() {
+        return {
+            todoValue: "123"
+        }
+    }
+})
+</script>
