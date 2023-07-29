ddd88f26cbce460a39adbdea45888c7ecba03e52
SanjarKeySkills <forest2004@mail.ru>
2023-07-29 18:27:24 +0600

    add compo

---
+++ src/components/addCompo.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<input v-model="todoValue" type="text">
+		<button>App</button>
+	</div>
+</template>
+
+<script>
+
+export default (await import('vue')).defineComponent({
+	name: 'AddCompo',
+	data() {
+		return {
+			todoValue: "123"
+		}
+	}
+})
+</script>
