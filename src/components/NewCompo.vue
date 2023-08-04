3df8bcd0f2dd96ffceed53c2d92609da70b16611
SanjarKeySkills <forest2004@mail.ru>
2023-08-04 23:54:57 +0600

    add new compo to components

---
+++ src/components/NewCompo.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<input v-model="todoValue" type="text">
+		<button>Compo</button>
+	</div>
+</template>
+
+<script>
+
+export default (await import('vue')).defineComponent({
+	name: 'AddNewCompo',
+	data() {
+		return {
+			todoValue: "compo"
+		}
+	}
+})
+</script>
