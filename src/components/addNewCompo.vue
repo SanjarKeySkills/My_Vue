ed5848680d0cab7d1f263c67761a760613a9377f
SanjarKeySkills <forest2004@mail.ru>
2023-08-08 01:01:23 +0600

    add addnewcompo

---
+++ src/components/addNewCompo.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<input v-model="todoValue" type="text">
+		<button>App2</button>
+	</div>
+</template>
+
+<script>
+
+export default (await import('vue')).defineComponent({
+	name: 'AddTodo',
+	data() {
+		return {
+			todoValue: "assdd"
+		}
+	}
+})
+</script>
